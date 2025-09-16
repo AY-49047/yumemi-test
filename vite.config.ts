1f36e3b30f6373895388371587c24855a98cc43a
AY-49047 <asai.49047@intage.com>
2025-09-16 18:34:10 +0900

    vite.config..tsの修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-// vite.config.ts / vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// 例: リポジトリが my-react-app の場合
+// 例: リポジトリ名が my-react-app の場合
 export default defineConfig({
-  base: "/yumemi-test/",
   plugins: [react()],
+  base: "/yumemi-test/",
 });
