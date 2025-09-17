2a94132868e3b07116f4706f9720721f7127e131
AY-49047 <asai.49047@intage.com>
2025-09-17 17:45:48 +0900

    API接続修正

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,31 +3,27 @@ import "./App.css";
 // import {} from "../types";
 
 export default function App() {
+  const url = import.meta.env.VITE_API_BASE_URL;
+  const apiKey = import.meta.env.VITE_API_KEY;
   useEffect(() => {
     async function main() {
-      const url = import.meta.env.API_BASE_URL;
-      const apiKey = import.meta.env.API_KEY;
-
       try {
-        const res = await fetch(url, {
+        const res = await fetch(`${url}/api/v1/prefectures`, {
           method: "GET",
           headers: {
             Accept: "application/json",
             "X-API-KEY": apiKey,
           },
         });
-
         if (!res.ok) {
           throw new Error(`HTTP ${res.status} ${res.statusText}`);
         }
-
         const data = await res.json();
         console.log("APIレスポンス:", data);
       } catch (err) {
         console.error("エラー:", err);
       }
     }
-
     main();
   }, []);
 
